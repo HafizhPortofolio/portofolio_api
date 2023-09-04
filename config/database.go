b1108a8f2b043a9b4c4722d1e262f40890eeecc9
MafuSora <hafizh.maulana.ub@gmail.com>
2023-09-04 16:23:44 +0700

    make env file

--- config/database.go
+++ config/database.go
@@ -2,21 +2,32 @@ package config
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 
+	"github.com/joho/godotenv"
 	"github.com/m/helper"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-const (
-	host     = "db.kjmsxmqosydoptrignaj.supabase.co"
-	port     = 5432
-	user     = "postgres"
-	password = "@orang111357"
-	dbname   = "postgres"
-)
-
 func DatabaseConnection() *gorm.DB {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env File")
+	}
+
+	host := os.Getenv("HOST_DB")
+	init_port := os.Getenv("PORT_DB")
+	port, err2 := strconv.Atoi(init_port)
+	if err2 != nil {
+		log.Fatal("Error loading .env File")
+	}
+	user := os.Getenv("USERNAME_DB")
+	password := os.Getenv("PASSWORD_DB")
+	dbname := os.Getenv("DB_NAME_DB")
+
 	sqlInfo := fmt.Sprintf("user=%s password=%s host=%s port=%d dbname=%s sslmode=disable", user, password, host, port, dbname)
 	db, err := gorm.Open(postgres.Open(sqlInfo), &gorm.Config{})
 	helper.ErrorPanic(err)
